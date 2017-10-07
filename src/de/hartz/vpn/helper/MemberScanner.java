73bfe402a089f6d7dd3c5c1a1ba9d501cef815a3
Kai Hartz <k_hart21@uni-muenster.de>
2017-10-08 01:08:02 +0200

    Updated Memberscanner. Still not used yet.

--- src/de/hartz/vpn/helper/MemberScanner.java
+++ src/de/hartz/vpn/helper/MemberScanner.java
@@ -1,8 +1,10 @@
 package de.hartz.vpn.helper;
 
 import java.io.IOException;
-import java.net.*;
-import java.util.Enumeration;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.nio.charset.StandardCharsets;
 
 /**
  * Created by kaiha on 07.10.2017.
@@ -10,38 +12,79 @@ import java.util.Enumeration;
  */
 public class MemberScanner {
 
+    private static final String MEMBER_PREFIX = "EASY_PEASY_VPN:";
+
+    private static MemberScanner INSTANCE;
+    private String ownUserName;
+    private InetAddress broadcast;
+
+    private static boolean run = false;
+
+    // Deny access.
+    private MemberScanner(){}
+
+    /**
+     * Initialises this object with the necessary data.
+     * @param ownUserName
+     * @param broadcast
+     */
+    public static void init(String ownUserName, InetAddress broadcast) {
+        if (INSTANCE == null) {
+            INSTANCE = new MemberScanner(ownUserName, broadcast);
+        } else {
+            INSTANCE.ownUserName = ownUserName;
+            INSTANCE.broadcast = broadcast;
+        }
+
+        if (!run) {
+            run = true;
+            new Thread(new Server()).start();
+            new Thread(new Client()).start();
+        }
+    }
+
+    /**
+     * Returns the singelton instance. It needs to be initialised before!
+     * @return
+     */
+    public static MemberScanner getInstance() {
+        if (INSTANCE == null) {
+            throw new RuntimeException("MemberScanner not initialised.");
+        }
+        return INSTANCE;
+    }
+
+    private MemberScanner(String ownUserName, InetAddress broadcast) {
+        this.ownUserName = ownUserName;
+        this.broadcast = broadcast;
+    }
+
+    private void receivedMember(String ip, String name) {
+        // TODO: Notify callbacks..
+    }
+
     private static class Server implements Runnable {
-        DatagramSocket socket;
 
         @Override
         public void run() {
             try {
                 //Keep a socket open to listen to all the UDP trafic that is destined for this port
-                socket = new DatagramSocket(8888, InetAddress.getByName("0.0.0.0"));
+                DatagramSocket socket = new DatagramSocket(8888, InetAddress.getByName("0.0.0.0"));
                 socket.setBroadcast(true);
 
-                while (true) {
-                    System.out.println(getClass().getName() + ">>>Ready to receive broadcast packets!");
+                // TODO: Make clean stoppable.
+                while (INSTANCE.run) {
 
-                    //Receive a packet
+                    //Wait for a response
                     byte[] recvBuf = new byte[15000];
-                    DatagramPacket packet = new DatagramPacket(recvBuf, recvBuf.length);
-                    socket.receive(packet);
+                    DatagramPacket receivePacket = new DatagramPacket(recvBuf, recvBuf.length);
+                    socket.receive(receivePacket);
 
-                    //Packet received
-                    System.out.println(getClass().getName() + ">>>Discovery packet received from: " + packet.getAddress().getHostAddress());
-                    System.out.println(getClass().getName() + ">>>Packet received; data: " + new String(packet.getData()));
-
-                    //See if the packet holds the right command (message)
-                    String message = new String(packet.getData()).trim();
-                    if (message.equals("DISCOVER_FUIFSERVER_REQUEST")) {
-                        byte[] sendData = "DISCOVER_FUIFSERVER_RESPONSE".getBytes();
-
-                        //Send a response
-                        DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, packet.getAddress(), packet.getPort());
-                        socket.send(sendPacket);
-
-                        System.out.println(getClass().getName() + ">>>Sent packet to: " + sendPacket.getAddress().getHostAddress());
+                    //Check if the message is correct
+                    String message = new String(receivePacket.getData()).trim();
+                    if (message.contains(MEMBER_PREFIX)) {
+                        String memberName = message.substring(MEMBER_PREFIX.length());
+                        INSTANCE.receivedMember(receivePacket.getAddress().getHostAddress(), memberName);
                     }
                 }
             } catch (IOException ex) {
@@ -61,69 +104,25 @@ public class MemberScanner {
                 //Open a random port to send the package
                 DatagramSocket c = new DatagramSocket();
                 c.setBroadcast(true);
+                byte[] sendData = getInstance().ownUserName.getBytes(StandardCharsets.UTF_8);
 
-                // TODO: Write user name here.
-                byte[] sendData = "DISCOVER_FUIFSERVER_REQUEST".getBytes();
-
-                // TODO: Check why! Shouldnt do this it sends private data out of the vpn.
-                //Try the 255.255.255.255 first
-                try {
-                    DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, InetAddress.getByName("255.255.255.255"), 8888);
-                    c.send(sendPacket);
-                    System.out.println(getClass().getName() + ">>> Request packet sent to: 255.255.255.255 (DEFAULT)");
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-
-                // TODO: Just use broadcast address of vpn adapter.
-                // Broadcast the message over all the network interfaces
-                Enumeration interfaces = NetworkInterface.getNetworkInterfaces();
-                while (interfaces.hasMoreElements()) {
-                    NetworkInterface networkInterface = (NetworkInterface) interfaces.nextElement();
-
-                    if (networkInterface.isLoopback() || !networkInterface.isUp()) {
-                        continue; // Don't want to broadcast to the loopback interface
+                while(INSTANCE.run) {
+                    // Send the broadcast package!
+                    try {
+                        DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, INSTANCE.broadcast, 8888);
+                        c.send(sendPacket);
+                    } catch (Exception e) {
+                        e.printStackTrace();
                     }
-
-                    for (InterfaceAddress interfaceAddress : networkInterface.getInterfaceAddresses()) {
-                        InetAddress broadcast = interfaceAddress.getBroadcast();
-                        if (broadcast == null) {
-                            continue;
-                        }
-
-                        // Send the broadcast package!
-                        try {
-                            DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, broadcast, 8888);
-                            c.send(sendPacket);
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                        }
-
-                        System.out.println(getClass().getName() + ">>> Request packet sent to: " + broadcast.getHostAddress() + "; Interface: " + networkInterface.getDisplayName());
-                    }
+                    Thread.sleep(30000);
                 }
 
-                System.out.println(getClass().getName() + ">>> Done looping over all network interfaces. Now waiting for a reply!");
-
-                //Wait for a response
-                byte[] recvBuf = new byte[15000];
-                DatagramPacket receivePacket = new DatagramPacket(recvBuf, recvBuf.length);
-                c.receive(receivePacket);
-
-                //We have a response
-                System.out.println(getClass().getName() + ">>> Broadcast response from server: " + receivePacket.getAddress().getHostAddress());
-
-                //Check if the message is correct
-                String message = new String(receivePacket.getData()).trim();
-                if (message.equals("DISCOVER_FUIFSERVER_RESPONSE")) {
-                    //DO SOMETHING WITH THE SERVER'S IP (for example, store it in your controller)
-
-                }
-
-                //Close the port!
                 c.close();
+                //Close the port!
             } catch (IOException ex) {
                 ex.printStackTrace();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
         }
     }
